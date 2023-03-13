3e599449d45fcebb9e67ab546836da4fa6065dbe
Jackh-01 <70713567+Jackh-01@users.noreply.github.com>
2023-03-13 16:16:42 -0500

    comen
    come

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b, residuo;
+    cout<<"ingrese un numero: ";
+    cin>>a;
+    cout<<"ingrese otro numero: ";
+    cin>>b;
+    residuo=a%b;
+    cout<<"el residuo de la division es: " <<residuo<<endl;
+    return 0;
+}
